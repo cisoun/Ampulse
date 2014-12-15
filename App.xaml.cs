7e78f612640fcae2ffb7d603dae3a79f615e7e9a
Cyriaque Skrapits <cysoun@gmail.com>
2014-12-15 16:04:09 +0100

    Shows a window with an amp as background image

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ampulse
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const string NAME = "Ampulse";
+        public const string VERSION = "0.0.1";
+        public const string AUTHOR = "Cyriaque Skrapits";
+    }
+}
